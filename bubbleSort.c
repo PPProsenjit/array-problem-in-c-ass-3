c404cfdf9ccca43bb2fafdbc78bcb9747ef5acc6
PPProsenjite <prosenjitpappu03455@gmial.com>
2024-11-21 18:22:21 +0600

    bubble Sort in c

---
+++ bubbleSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+//swap algoritham 
+int swap(int* arr, int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int bubbleSort(int arr[], int n){
+    //loop for array
+ for(int i = 0; i < n-1; i++){
+     //i is already in so the loop will only num n-i-1
+     for(int j = 0; j < n - i - 1; j++){
+         //compare left to right which one is greater than
+         if(arr[j] > arr[j + 1]){
+             //calll swap algoritham
+             swap(arr, j, j + 1);
+         }
+     }
+ }    
+}
+
+int main() {
+    int arr[] = {1, 5, 6, 8, 2, 0, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    for(int j; j < n; j++){
+        printf("%d ", arr[j]);
+    }
+
+    return 0;
+}
